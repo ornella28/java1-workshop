9f3d6f6e2bb170333d7218b53c26793c09515c52
ornella28 <angelnibaruta88@gmail.com>
2025-11-26 12:38:56 +0100

    right code for loop creation

--- src/main/java/se/lexicon/calculatorApplication.java
+++ src/main/java/se/lexicon/calculatorApplication.java
@@ -8,49 +8,47 @@ public class calculatorApplication {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the first number: ");
-        double number1 = scanner.nextInt();
 
-        System.out.println("Please enter the second number: ");
-        double number2 = scanner.nextInt();
+        String answer;//for storing yes or no
 
-        System.out.println("Please choose the operation: " + " + " + " - " + " * " + " / ");
-        char operation = scanner.next().charAt(0);
+        do { // the program will perform this code as long as the answer will be yes
 
-        double result = 0;
-        switch (operation) {
-            case '+':
-                result = number1 + number2;
-                break;
+            System.out.println("Please enter the first number: ");
+            double number1 = scanner.nextInt();
+
+            System.out.println("Please enter the second number: ");
+            double number2 = scanner.nextInt();
+
+            System.out.println("Please choose the operation: " + " + " + " - " + " * " + " / ");
+            char operation = scanner.next().charAt(0); //to store the mathematic operation
+
+            double result = 0;
+            switch (operation) {
+                case '+':
+                    result = number1 + number2;
+                    break;
                 case '-':
                     result = number1 - number2;
                     break;
-                    case '*':
-                        result = number1 * number2;
-                        break;
-                        case '/':
-                            result = number1 / number2;
-                            break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    break;
 
-        }
-        System.out.println("The result is: " + result);
-
-
-
-        boolean isContinue = true;
-        while (isContinue) {
+            }
+            System.out.println("The result is: " + result);
 
             System.out.println("Do you want to continue? (yes/no):");
-            String answer = scanner.nextLine();//no
-            if (answer.equalsIgnoreCase("no")) {
-                isContinue = false;
-            }
+            answer = scanner.next();
 
 
+        }  while (answer.equalsIgnoreCase("yes"));
+            //isContinue = false;
+            System.out.println("Thank you, see you later!");// will print this message if the answer is no
 
 
-        }
-        System.out.println("Thank you, see you later!");
 
     }
 }
