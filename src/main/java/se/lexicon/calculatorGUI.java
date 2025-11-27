19aea4f3772e78ccbb258af57f952e63e4417441
ornella28 <angelnibaruta88@gmail.com>
2025-11-27 14:53:34 +0100

    some notes

--- src/main/java/se/lexicon/calculatorGUI.java
+++ src/main/java/se/lexicon/calculatorGUI.java
@@ -11,20 +11,20 @@ public class calculatorGUI {
 
     public static void main(String[] args) {
 
-
+// creates a frame
         JFrame frame = new JFrame("Calculator");
         frame.setSize(500,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
-
+// creates a field for number 1
         JTextField number1Field = new JTextField();
         number1Field.setBounds(40,20,100,50);// otherwise the frame will be visible but nothing inside
         frame.add(number1Field);
-
+// field for number 2, position it
         JTextField number2Field = new JTextField();
         number2Field.setBounds(150,20,100,50); //setBounds helps make the fields visible
         frame.add(number2Field);
-
+//label
         JLabel resultLabel = new JLabel("Result:");
         resultLabel.setBounds(50,130,250,25);
         frame.add(resultLabel);
@@ -49,10 +49,9 @@ public class calculatorGUI {
 
 
         additionButton.addActionListener(new ActionListener() { // this will perform additions and put result in result
-
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
+                try { // bo not crash if i enter invalid number
                 double a = Double.parseDouble(number1Field.getText());
                 double b = Double.parseDouble(number2Field.getText());
                 resultLabel.setText("Result:" + (a + b));
@@ -100,7 +99,7 @@ public class calculatorGUI {
         });
 
 
-        frame.setVisible(true);
+        frame.setVisible(true); // we are able to see the frame
 
     }
 }
